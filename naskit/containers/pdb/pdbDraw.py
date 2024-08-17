c646918782c810e43fc7d02e32113ccfcfc2a9d8
Artem <artem.liamkrow@gmail.com>
2024-08-17 15:08:04 +0300

    add matplotlib support

--- naskit/containers/pdb/pdbDraw.py
+++ naskit/containers/pdb/pdbDraw.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Optional
 
 
 
@@ -7,23 +8,14 @@ class PDBDraw:
     default_color = "#111"
     atom_radius_map = {"C":0.67, "H":0.53, "S":0.88, "O":0.48, "N":0.56, "P":0.98}
     default_radius = 0.75
-    
-    def _marker_size_multiplier(self, natoms):
-        return 100*np.exp(-(2.e-2*natoms)**.5) + 10
-        
-    def draw(
-            self,
-            width=600,
-            height=500,
-            size_m=None,
-            draw_hydrogens=True,
-            show_axis=False
-            ):
-        
-        import plotly.graph_objects as go
-        
+
+
+    def _draw_params(self, 
+                     draw_hydrogens: bool = True, 
+                     size_m: bool = None
+                    ):
         if size_m is None:
-            size_m = self._marker_size_multiplier(self.natoms)
+            size_m = 100*np.exp(-(2.e-2*self.natoms)**.5) + 10
         
         if draw_hydrogens:
             c = self.coords
@@ -36,16 +28,55 @@ class PDBDraw:
             size = [self.atom_radius_map.get(a.element, self.default_radius)*size_m for a in self.atoms() if a.element!='H']
             name = [f"{a.name} {a.moln} {a.mol_name}" for a in self.atoms() if a.element!='H']
 
-        scatter_go = go.Scatter3d(x=c[:, 0], y=c[:, 1], z=c[:, 2], 
-                                  hoverinfo="text",
-                                  hovertext=name,
-                                  mode='markers', 
-                                  marker=dict(size=size, color=color, opacity=1.)
-                                 )
-        fig = go.Figure(data=[scatter_go], layout=go.Layout(width=width, height=height))
-        fig.update_scenes(dragmode="orbit")
-        if not show_axis:
-            fig.update_scenes(xaxis_visible=False, 
-                              yaxis_visible=False, 
-                              zaxis_visible=False)
-        fig.show()
+        return {'coords':c, 'colors':color, 'sizes':size, 'names':name}
+        
+        
+    def draw(
+            self,
+            width: int = 600,
+            height: int = 500,
+            size_m: int = None,
+            draw_hydrogens: bool = True,
+            show_axis: bool = False,
+            matplot: bool = False
+            ):
+        
+        params = self._draw_params(draw_hydrogens, size_m)
+        xs, ys, zs = params['coords'][:, 0], params['coords'][:, 1], params['coords'][:, 2]
+
+        if matplot:
+            import matplotlib.pyplot as plt
+            fig = plt.figure(figsize=(width/100, height/100))
+            ax = fig.add_subplot(projection='3d')
+            if not show_axis:
+                ax.set_axis_off()
+                
+            ax.scatter(xs, ys, zs, 
+                       s=[8*s for s in params['sizes']], 
+                       c=params['colors'],
+                       alpha=1.
+                      )
+            ax.set_aspect('equal', 'box')
+
+        else:
+            import plotly.graph_objects as go
+            scatter_go = go.Scatter3d(x=xs, y=ys, z=zs, 
+                                      hoverinfo="text",
+                                      hovertext=params['names'],
+                                      mode='markers', 
+                                      marker=dict(
+                                          size=params['sizes'], 
+                                          color=params['colors'], 
+                                          opacity=1.
+                                      )
+                                     )
+            fig = go.Figure(data=[scatter_go], layout=go.Layout(width=width, height=height))
+            fig.update_scenes(dragmode="orbit", aspectmode="cube", aspectratio=dict(x=1, y=1, z=1))
+            if not show_axis:
+                fig.update_scenes(xaxis_visible=False, 
+                                  yaxis_visible=False, 
+                                  zaxis_visible=False)
+            fig.show()
+
+
+
